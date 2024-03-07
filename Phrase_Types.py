8f414207d4daf4c3d9b9beeeffa2af9b522f1497
Xam.N <maximilian.nedbalek@students.mq.edu.au>
2024-03-08 10:50:49 +1100

    Phrase Type Layout

---
+++ Phrase_Types.py
@@ -0,0 +1,17 @@
+#use tuple()
+
+#Boolen
+#'unit' + 'bool' + 'unit'
+
+#credit point
+#xxcp from 'level' units
+#xxcp from 'inequality' 'level'
+#xxcp from 'subject' at 'level'
+#xxcp from 'unit range'
+
+#composite
+#combination of other phrase types
+
+#other
+#special admission required etc.
+#ignore everything that reaches here
