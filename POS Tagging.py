537b33b60eb54a60fc44eea3693ff7a1dcfa3af1
Armlete <maximilian.nedbalek@students.mq.edu.au>
2024-03-14 21:52:25 +1100

    PC switch

---
+++ POS Tagging.py
@@ -0,0 +1,14 @@
+from Data_Processing import dataRead
+
+import nltk
+import re
+#tokenise description so that i can look at each word
+
+#tag each word with a thingy\
+
+def tagDescription(data):
+  words = nltk.word_tokenise(data)
+  for word in words:
+    if word
+    if word 
+  
